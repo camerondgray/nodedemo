8f8513151ecbfc671acefb08793d609ce3be50c5
camerongray <868953+camerondgray@users.noreply.github.com>
2019-12-03 10:55:05 -0800

    testing review apps

--- app.js
+++ app.js
@@ -2,23 +2,11 @@ var redis = require('redis')
 const express = require('express')
 const app = express()
 const port = 3000
-var redis_url = process.env.DATABASE_URL
 
-var client = redis.createClient(redis_url)
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-client.on("error", function (err) {
-    console.log("Error " + err);
-});
-
-client.on("connect", function(){
-    client.set("1", "Hello From Convox", redis.print)
-    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-})
 
 
 app.get('/', function(req, res){
-    var response
-    client.get("1", function(error, result){
-        res.send(result)
-    })
+    res.send("hello review app");
 })
